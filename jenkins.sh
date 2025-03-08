3f4e609010ebd88be5c64c91d9e16a45f6e8d475
elvispronda <elvisndayishimiye200@gmail.com>
2025-03-08 09:12:43 +0200

    Update jenkins settings
    adding configuration on jenkins files

--- jenkins.sh
+++ jenkins.sh
@@ -1,17 +1,22 @@
 #STEP-1: INSTALLING GIT JAVA-1.8.0 MAVEN
-yum install git java-1.8.0-openjdk maven -y
+sudo apt update && sudo apt upgrade -y
+sudo apt install git  openjdk-8-jdk maven -y
+
 
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo apt update && sudo apt upgrade -y
+wget -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 
 #STEP-3: DOWNLOAD JAVA11 AND JENKINS
-sudo yum install java-17-amazon-corretto -y
-yum install jenkins -y
+sudo apt update && sudo apt upgrade -y
+sudo apt install fontconfig openjdk-17-jre -y
+sudo apt install jenkins -y
 update-alternatives --config java
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
-systemctl start jenkins.service
-systemctl status jenkins.service
+sudo systemctl enable --now jenkins
+sudo systemctl status jenkins
 #This command allows jenkins service to stay on even when the server is off
 chconfig jenkins on 
